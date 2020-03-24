930a624ea2368a83638cff65d5082d725d552d6c
Jonathan Souza <jonathan.wdev@gmail.com>
2020-03-24 11:40:33 -0300

    first commit

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,27 @@
+const crypto = require("crypto");
+const connection = require("../database/connection");
+
+class OngController {
+
+  async index(req,res) {
+    const ongs = await connection("ongs").select("*");
+    return res.json(ongs);
+  }
+
+  async store(req,res) {    
+    const { name, email, city, uf, whatsapp } = req.body;
+    const id = crypto.randomBytes(4).toString("HEX");    
+    await connection("ongs").insert({
+      id,
+      name, 
+      email, 
+      city, 
+      uf, 
+      whatsapp
+    });  
+    return res.json({id});
+  } 
+}
+
+
+module.exports = new OngController();
